38bfcade19735d28ce5279055cb45399f0367fcd
geozasimov <geozasimov@gmail.com>
2021-08-29 17:15:23 +0300

    some attemps

--- main.cpp
+++ main.cpp
@@ -2,6 +2,39 @@
 
 int main()
 {
-	printf("Hello, onegin")
+	///smth
 	return 0;
+}
+int Puts(char* str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	printf("\n");
+
+	return 0;
+	}
+
+char* StrChr(char *str, int c)
+{
+	while(*str != '\0')
+	{
+		if (*str == 'c')
+		return str;
+	}
+	return NULL;
+	}
+
+int StrLen(char* str)
+{
+	int i;
+	int n = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		n++;
+	}
+	return n;
 }
